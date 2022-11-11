26b71ae4adc4cf99714f35b1a4b2ee6f769e725b
sumedh raj <sumedh@tealfeed.com>
2022-11-11 15:37:33 +0530

    Added ANTD and Header

---
+++ components/editor/config/Toolbar/index.tsx
@@ -0,0 +1,158 @@
+/* eslint-disable */
+import React, { useContext, useEffect } from "react";
+import { Tooltip } from "antd";
+import {
+  BlockToolbarButton,
+  ELEMENT_BLOCKQUOTE,
+  ELEMENT_H1,
+  ELEMENT_H2,
+  ELEMENT_OL,
+  ELEMENT_UL,
+  getPluginType,
+  ListToolbarButton,
+  MarkToolbarButton,
+  MARK_BOLD,
+  MARK_CODE,
+  MARK_ITALIC,
+  MARK_UNDERLINE,
+  usePlateEditorRef,
+  useEventPlateId,
+} from "@udecode/plate";
+import { ReactEditor } from "slate-react";
+// import HeadingIcon from "@/../../public/H1.svg";
+// import QuoteIcon from "@/../../public/blockquote.svg";
+// import HeadingTwoIcon from "@/../../public/h2.svg";
+// import CodeIcon from "@/../../public/codeblock.svg";
+// import InlineCodeIcon from "@/../../public/inlinecode.svg";
+// import OLIcon from "@/../../public/OL.svg";
+// import ULIcon from "@/../../public/UL.svg";
+// import BoldIcon from "@/../../public/Bold.svg";
+// import ItalicIcon from "@/../../public/italics.svg";
+// import UnderlineIcon from "@/../../public/underline.svg";
+// import LinkIcon from "@/../../public/link.svg";
+// import EmojiButton from "./EmojiPanel";
+// import Shortcuts from "../elements/shortcuts";
+// import { ToolbarCodeBlock } from "../elements/ToolBarCodeBlock";
+// import { EditorContext } from "@/Context/EditorContext";
+
+let OSName: string | undefined;
+if (typeof navigator !== "undefined") {
+  if (navigator.appVersion.indexOf("Mac") != -1) {
+    OSName = "Mac";
+  } else {
+    OSName = "Win";
+  }
+}
+
+export const ToolbarButtonsBasicElements = React.memo(() => {
+  const editor = usePlateEditorRef(useEventPlateId("focus"));
+
+  return (
+    <>
+      <BlockToolbarButton
+        type={getPluginType(editor, ELEMENT_H1)}
+        icon={
+          <div>
+            <h1>H1</h1>
+          </div>
+        }
+      />
+      <BlockToolbarButton
+        type={getPluginType(editor, ELEMENT_H2)}
+        icon={<h1>H2</h1>}
+      />
+      <BlockToolbarButton
+        type={getPluginType(editor, ELEMENT_BLOCKQUOTE)}
+        icon={<h1>Q</h1>}
+      />
+      {/* <ToolbarCodeBlock
+        icon={
+          <Tooltip
+            getPopupContainer={(trigger) =>
+                                  trigger.parentNode as HTMLElement
+                                }
+            title="Code-block( ``` )"
+          >
+            <CodeIcon />
+          </Tooltip>
+        }
+      /> */}
+      <MarkToolbarButton
+        type={getPluginType(editor, MARK_CODE)}
+        icon={<h1>Inline</h1>}
+      />
+    </>
+  );
+});
+
+export const ToolbarButtonsList = React.memo(() => {
+  const editor = usePlateEditorRef(useEventPlateId("focus"));
+
+  return (
+    <>
+      <ListToolbarButton
+        type={getPluginType(editor, ELEMENT_UL)}
+        icon={<h1>UL</h1>}
+      />
+      <ListToolbarButton
+        type={getPluginType(editor, ELEMENT_OL)}
+        icon={<h1>OL</h1>}
+      />
+    </>
+  );
+});
+
+export const ToolbarButtonsBasicMarks = React.memo(() => {
+  const editor = usePlateEditorRef(useEventPlateId("focus"));
+
+  return (
+    <>
+      <MarkToolbarButton
+        type={getPluginType(editor, MARK_BOLD)}
+        icon={<h1>B</h1>}
+      />
+      <MarkToolbarButton
+        type={getPluginType(editor, MARK_ITALIC)}
+        icon={<h1>I</h1>}
+      />
+      <MarkToolbarButton
+        type={getPluginType(editor, MARK_UNDERLINE)}
+        icon={<h1>U</h1>}
+      />
+    </>
+  );
+});
+
+export const ToolbarButtons = React.memo(() => {
+  const editor = usePlateEditorRef(useEventPlateId("focus"));
+  //   const { setLinkLocation }: any = useContext(EditorContext);
+
+  // useEffect(() => {
+  //   ReactEditor.focus(editor);
+  // }, []);
+
+  function handleLink() {
+    // setLinkLocation(editor.selection);
+    console.log("Hello");
+  }
+
+  return (
+    <>
+      <ToolbarButtonsBasicElements />
+      <ToolbarButtonsList />
+      <ToolbarButtonsBasicMarks />
+      <button onClick={handleLink}>
+        {/* <Tooltip
+          getPopupContainer={(trigger) =>
+                                  trigger.parentNode as HTMLElement
+                                }
+          title="Link"
+        >
+          <LinkIcon />
+        </Tooltip> */}
+      </button>
+      {/* <EmojiButton />
+      <Shortcuts /> */}
+    </>
+  );
+});
--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -17,6 +17,7 @@ import {
   createDeserializeCsvPlugin,
   Plate,
   createSoftBreakPlugin,
+  HeadingToolbar,
 } from "@udecode/plate";
 import { createJuicePlugin } from "@udecode/plate-juice";
 import { editableProps } from "./common/editableProps";
@@ -27,6 +28,7 @@ import { indentPlugin } from "./plugins/IndentPlugin";
 import { plateUI } from "./common/plateUi";
 import { basicNodesPlugins } from "./plugins/BasicNodesPlugin";
 import { softBreakPlugin } from "./plugins/SoftBreakPlugin";
+import { ToolbarButtons } from "./config/Toolbar";
 
 const plugins = createMyPlugins(
   [
@@ -60,7 +62,19 @@ const NewEditor = () => (
     editableProps={editableProps}
     plugins={plugins}
     onChange={(e) => console.log(e)}
-  />
+  >
+    <div className="z-10 fixed top-0 left-0 w-full bg-red-400 pt-2">
+      <HeadingToolbar
+        style={{
+          border: "none",
+        }}
+      >
+        <div className="w-1/2 mx-auto z-10 rounded-lg flex items-center justify-around flex-wrap pt-12 xl:pt-0">
+          <ToolbarButtons />
+        </div>
+      </HeadingToolbar>
+    </div>
+  </Plate>
 );
 
 export default NewEditor;
