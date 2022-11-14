250784d3b7bd8b64f7053cbbae172be7e3d3b349
sumedh raj <sumedh@tealfeed.com>
2022-11-14 11:34:03 +0530

    Imported Previous styles

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -58,23 +58,28 @@ const plugins = createMyPlugins(
 );
 
 const NewEditor = () => (
-  <Plate<MyValue>
-    editableProps={editableProps}
-    plugins={plugins}
-    onChange={(e) => console.log(e)}
+  <div
+    className="mx-auto my-9 w-full h-5/6 md:w-6/6 pb-4 px-2"
+    id="rich-editor-tealfeed"
   >
-    <div className="z-10 fixed top-0 left-0 w-full bg-red-400 pt-2">
-      <HeadingToolbar
-        style={{
-          border: "none",
-        }}
-      >
-        <div className="w-1/2 mx-auto z-10 rounded-lg flex items-center justify-around flex-wrap pt-12 xl:pt-0">
-          <ToolbarButtons />
-        </div>
-      </HeadingToolbar>
-    </div>
-  </Plate>
+    <Plate<MyValue>
+      editableProps={editableProps}
+      plugins={plugins}
+      onChange={(e) => console.log(e)}
+    >
+      <div className="z-10 fixed top-0 left-0 w-full pt-2">
+        <HeadingToolbar
+          style={{
+            border: "none",
+          }}
+        >
+          <div className="w-1/2 mx-auto z-10 rounded-lg flex items-center justify-around flex-wrap pt-12 xl:pt-0 bg-red-400">
+            <ToolbarButtons />
+          </div>
+        </HeadingToolbar>
+      </div>
+    </Plate>
+  </div>
 );
 
 export default NewEditor;
